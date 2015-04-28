71ee2f58c4faba761777fb5d3e1547bb3f2e67e1
janiocauper <janio_cauper@hotmail.com>
2015-04-28 14:17:14 -0300

    Delete TesteCelular.c

--- TesteCelular.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(){
-	printf("Teste commit pelo celular\n");
-<<<<<<< HEAD
-	printf("Segundo commit pelo celular\n");
-=======
->>>>>>> branch 'master' of https://github.com/janiocauper/Prog-II.git
-}
